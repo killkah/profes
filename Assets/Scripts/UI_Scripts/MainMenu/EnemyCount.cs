fab9eeaffcc42b7cab14ee994aa506e4ac3d3fac
killkah <yuyarik5@gmail.com>
2024-06-18 18:38:33 +0700

    vertwo

--- Assets/Scripts/UI_Scripts/MainMenu/EnemyCount.cs
+++ Assets/Scripts/UI_Scripts/MainMenu/EnemyCount.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 public class EnemyCount : MonoBehaviour
 {
     [SerializeField] private Dropdown enemyCount;
+
     [SerializeField] private GameObject[] gameObjects;
     [SerializeField] private GameObject enemySetPrefab;
     [SerializeField] private GameObject enemySetParent;
 
+    void Start()
+    {
+        //Invoke("CreateEnemy", 0.05f);
+        CreateEnemy();
+    }
     public void CreateEnemy()
     {
-        
+        Color[] colors = { Color.red, Color.yellow, Color.gray, Color.black, Color.blue, Color.cyan, Color.green };
         gameObjects = GameObject.FindGameObjectsWithTag("EnemySet");
         for (int i = 0; i < gameObjects.Length; i++)
         {
@@ -21,8 +27,12 @@ public class EnemyCount : MonoBehaviour
         int count = enemyCount.value + 1;
         for (int i = 0; i < count; i++)
         {
-            Instantiate(enemySetPrefab, enemySetParent.transform);
+            GameObject newEnemy = Instantiate(enemySetPrefab, enemySetParent.transform);
+            InputField name = newEnemy.GetComponentInChildren<InputField>();
+            Image colorEnemy = newEnemy.GetComponentInChildren<Button>().gameObject.GetComponent<Image>();
+            name.text = "Противник " + (i + 1);
+            colorEnemy.color = colors[i];
         }
-            
+        LayoutRebuilder.ForceRebuildLayoutImmediate(enemySetParent.GetComponent<RectTransform>());   
     }
 }
--- Assets/Scripts/UI_Scripts/MainMenu/ScreenSettings.cs
+++ Assets/Scripts/UI_Scripts/MainMenu/ScreenSettings.cs
@@ -2,43 +2,98 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using System;
+using System.IO;
 using UnityEngine.UI;
 
+[Serializable]
+public class Settings
+{
+    public int resolutionValue;
+    public bool fullScreen;
+    public bool on_Sound;
+    public float volume;
+}
 public class ScreenSettings : MonoBehaviour
 {
     [SerializeField] private Dropdown resolutionParametr;
-    [SerializeField] private bool onFullScreen;
+    [SerializeField] private Toggle onFullScreen;
+    [SerializeField] private Toggle onSounds;
+    [SerializeField] private Slider volume;
+
+
+
+    [SerializeField] private GameObject dangerScreen;
+    [SerializeField] private GameObject settingsMenu;
+
+    public Settings settings = new Settings();
     // Start is called before the first frame update
     void Start()
     {
-        
+        string json = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "Settings.json"));
+        settings = JsonUtility.FromJson<Settings>(json);
+        resolutionParametr.value = settings.resolutionValue;
+        onFullScreen.isOn = settings.fullScreen;
+        onSounds.isOn = settings.on_Sound;
+        volume.value = settings.volume;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
-    public void ChangedValue()
+    public void SaveSettings()
     {
-        if(resolutionParametr.value == 0)
+        settings.resolutionValue = resolutionParametr.value;
+        settings.fullScreen = onFullScreen.isOn;
+        settings.on_Sound = onSounds.isOn;
+        settings.volume = volume.value;
+        string json = JsonUtility.ToJson(settings);
+        File.WriteAllText(Path.Combine(Application.streamingAssetsPath, "Settings.json"), json);
+
+        if (settings.resolutionValue == 0)
         {
-            Screen.SetResolution(1280, 720, onFullScreen);
+            Screen.SetResolution(1280, 720, settings.fullScreen);
             print("1280:720");
         }
-        else if(resolutionParametr.value == 1)
+        else if(settings.resolutionValue == 1)
         {
-            Screen.SetResolution(1366, 768, onFullScreen);
+            Screen.SetResolution(1366, 768, settings.fullScreen);
             print("1366:768");
         }
-        else if (resolutionParametr.value == 2)
+        else if (settings.resolutionValue == 2)
         {
-            Screen.SetResolution(1600, 900, onFullScreen);
+            Screen.SetResolution(1600, 900, settings.fullScreen);
             print("1600:900");
         }
-        else if (resolutionParametr.value == 3)
+        else if (settings.resolutionValue == 3)
         {
-            Screen.SetResolution(1920, 1080, onFullScreen);
+            Screen.SetResolution(1920, 1080, settings.fullScreen);
             print("1920:1080");
         }
+        settingsMenu.SetActive(false);
+    }
+    public void ExitSettings()
+    {
+        if (settings.resolutionValue != resolutionParametr.value || settings.fullScreen != onFullScreen.isOn || settings.on_Sound != onSounds.isOn || settings.volume != volume.value)
+        {
+            dangerScreen.SetActive(true);
+            
+        }
+        else
+        {
+            settingsMenu.SetActive(false);
+        }
+    }
+    public void RemoveSettings()
+    {
+        string json = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "Settings.json"));
+        settings = JsonUtility.FromJson<Settings>(json);
+        resolutionParametr.value = settings.resolutionValue;
+        onFullScreen.isOn = settings.fullScreen;
+        onSounds.isOn = settings.on_Sound;
+        volume.value = settings.volume;
+        dangerScreen.SetActive(false);
     }
 }
